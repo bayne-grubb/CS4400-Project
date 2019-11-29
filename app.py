5bf6e5c9d90b2a0052adf3e08dc3a943ced213dd
kroman6 <kroman6@gatech.edu>
2019-11-29 03:27:09 -0500

    adding

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
-from flask import Flask
+from flask import Flask, redirect, render_template, request
+
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
-    return render_template('layout.html')
+    return render_template('login.html')
+@app.route("/register")
+def register():
+    return render_template(url_for('register-navigation'))
+
+
+
 
 
 
